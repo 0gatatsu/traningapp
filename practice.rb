bd962651ac70542a50d431ef51d073e6f03b182c
0gatatsu <154909437+0gatatsu@users.noreply.github.com>
2024-06-17 23:28:08 +0900

    mixjuiceメソッド練習

--- practice.rb
+++ practice.rb
@@ -39,3 +39,13 @@ end
 def say_hello
   puts "Hello world"
 end
+
+def mixer(fruit)
+  puts "#{fruit}を細かく砕く"
+  return"#{fruit}ジュース"
+end
+
+puts "フルーツを入力してください"
+input = gets.chomp
+
+puts mixer(input)
